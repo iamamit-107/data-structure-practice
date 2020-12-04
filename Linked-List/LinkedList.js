e28063e10072d21a977d2d2b98837b7dd0ad2e02
iamamit-107 <ahmed.amit107@gmil.com>
2020-12-04 22:09:33 +0600

    linked list implemented

--- Linked-List/LinkedList.js
+++ Linked-List/LinkedList.js
@@ -49,18 +49,117 @@ class LinkedList {
     clear() {
         this.head = null;
     }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        let node = this.head;
+        this.head = node.next;
+    }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+        if (!this.head.next) {
+            this.head = null;
+            return;
+        }
+
+        let previous = this.head;
+        let node = this.head.next;
+
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+
+        previous.next = null;
+    }
+
+    insertLast(data) {
+        let node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+        }
+
+        let currentNode = this.head;
+        while (currentNode.next) {
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = node;
+    }
+
+    getAt(index) {
+        if (!this.head) {
+            return null;
+        }
+
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+            counter++;
+            node = node.next;
+        }
+
+        return null;
+    }
+
+    removeAt(index) {
+        if (!this.head) {
+            return null;
+        }
+
+        if (index === 0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        let previous = this.getAt(index - 1);
+        if (!previous || !previous.next) {
+            return;
+        }
+
+        previous.next = previous.next.next;
+    }
+
+    insertAt(data, index) {
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        let previous = this.getAt(index - 1) || this.getLast();
+        let node = new Node(data, previous.next);
+        previous.next = node;
+    }
 }
 
 const node = new Node(5);
 const list = new LinkedList();
 list.head = node;
 list.insertFirst(10);
-list.insertFirst(15);
-list.insertFirst(20);
-
-console.log(list);
-console.log(list.size());
+list.insertLast(0);
+// list.removeLast();
 console.log(list.getFirst());
-console.log(list.getLast());
-list.clear();
 console.log(list.size());
+console.log(list.getAt(10));
+// list.removeFirst();
+
+// console.log(list.size());
+// console.log(list.getFirst());
+// console.log(list.getLast());
+//list.clear();
+// console.log(list.size());
